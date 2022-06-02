a103b91f8662f2b2a45af82ad12a91cad8955c07
AnthonyArenas <u201410574@upc.edu.pe>
2022-06-01 19:27:20 -0500

    Project Codega Springboot

---
+++ Codega/src/main/java/pe/edu/upc/Codega/CodegaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.Codega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodegaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodegaApplication.class, args);
+	}
+
+}
